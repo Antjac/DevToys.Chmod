3f3afb7d9ea15d4af1ff41ac6df2e124d96794fd
Antjac <antjac.tux@free.fr>
2024-04-26 00:09:44 +0200

    Ajoutez des fichiers projet.

---
+++ ChmodEncoderDecoderGui.cs
@@ -0,0 +1,28 @@
+﻿using Chmod;
+using DevToys.Api;
+using System.ComponentModel.Composition;
+using static DevToys.Api.GUI;
+
+namespace Chmod;
+
+[Export(typeof(IGuiTool))]
+[Name("ChmodEncoderDecoder")]                                                        
+[ToolDisplayInformation(
+	IconFontName = "FluentSystemIcons",                                       
+	IconGlyph = '\uE670',                                                     
+	GroupName = PredefinedCommonToolGroupNames.EncodersDecoders,             
+	ResourceManagerAssemblyIdentifier = nameof(MyResourceAssemblyIdentifier),
+	ResourceManagerBaseName = "Chmod.ChmodEncoderDecoder",
+	ShortDisplayTitleResourceName = nameof(ChmodEncoderDecoder.ShortDisplayTitle),
+	LongDisplayTitleResourceName = nameof(ChmodEncoderDecoder.LongDisplayTitle),
+	DescriptionResourceName = nameof(ChmodEncoderDecoder.Description),
+	AccessibleNameResourceName = nameof(ChmodEncoderDecoder.AccessibleName))]
+internal sealed class ChmodEncoderDecoderGui : IGuiTool
+{
+	public UIToolView View => new(Label().Style(UILabelStyle.BodyStrong).Text(ChmodEncoderDecoder.HelloWorldLabel));
+
+	public void OnDataReceived(string dataTypeName, object? parsedData)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ MyResourceAssemblyIdentifier.cs
@@ -0,0 +1,14 @@
+﻿using DevToys.Api;
+using System.ComponentModel.Composition;
+
+namespace Chmod;
+
+[Export(typeof(IResourceAssemblyIdentifier))]
+[Name(nameof(MyResourceAssemblyIdentifier))]
+internal sealed class MyResourceAssemblyIdentifier : IResourceAssemblyIdentifier
+{
+	public ValueTask<FontDefinition[]> GetFontDefinitionsAsync()
+	{
+		throw new NotImplementedException();
+	}
+}
